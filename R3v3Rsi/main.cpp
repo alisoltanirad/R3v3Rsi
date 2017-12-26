ec96fe8f4609037ae21cb7bc6dc65ad8f066cba8
Ali Soltani Rad <soltaniradali@gmail.com>
2017-12-26 04:09:55 +0330

    building project

---
+++ R3v3Rsi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  R3v3Rsi
+//
+//  Created by Ali Soltani Rad on 12/26/17.
+//  Copyright © 2017 Ali Soltani Rad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
