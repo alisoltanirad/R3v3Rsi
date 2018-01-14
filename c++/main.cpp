c0281fcdf99c13505af5961b3fa31ae0be431535
Ali Soltani Rad <soltaniradali@gmail.com>
2018-01-14 22:57:27 +0330

    Add makeboard function to C++ file

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 
+void makeBoard(int (&board)[8][8]){
+    for (int i=0; i<8; i++){
+        for (int j=0; j<8; j++){
+            board[i][j] = 0;
+        }
+    }
+    board[3][3] = board[4][4] = 1;
+    board[3][4] = board[4][3] = -1;
+}
+
 int main(){
-    cout << "test";
+
+    int board[8][8];
+    makeBoard(board);
+
     return 0;
 }
